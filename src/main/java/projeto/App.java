747470d93afe866568e037560b68228629ba72b8
Murillo <murillogodoi13@hotmail.com>
2021-03-23 22:41:19 -0300

    add examples in main class

--- src/main/java/projeto/App.java
+++ src/main/java/projeto/App.java
@@ -9,9 +9,24 @@ public class App
     public static void main( String[] args )
     {
         Cliente murillo = new Cliente(1, "Murillo Godoi", "27/12/2000");
-        Conta murilloConta = new Conta(2000.00, 123, murillo);
         System.out.println(murillo.toString());
         System.out.println(" ");
-        System.out.println(murilloConta);
+
+        System.out.println(" ");
+
+        ContaCorrente murilloContaCorrente = new ContaCorrente(2500.00, 1234, murillo, 3000.00);
+        System.out.println(murilloContaCorrente.getSaldo());
+        murilloContaCorrente.depositar(1000.00);
+        System.out.println(murilloContaCorrente.getSaldo());
+        ContaPoupanca murilloContaPoupanca = new ContaPoupanca(3000.00, 12345, murillo,"23/03/2021");
+        System.out.println(murilloContaPoupanca.getSaldo());
+        murilloContaPoupanca.depositar(1000.00);
+        System.out.println(murilloContaPoupanca.getSaldo());
+    
+
+        System.out.println(murilloContaCorrente);
+        System.out.println(" ");
+        System.out.println(murilloContaPoupanca);
+
     }
 }
