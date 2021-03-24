05eec9aa81e966558d3d1d0cd9775bca97afce69
Murillo <murillogodoi13@hotmail.com>
2021-03-23 21:18:55 -0300

    Create cliente class

---
+++ src/main/java/projeto/Cliente.java
@@ -0,0 +1,31 @@
+package projeto;
+
+public class Cliente {
+  private Integer id;
+  private String nome;
+  private String dataNascimento;
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getDataNascimento() {
+    return dataNascimento;
+  }
+
+  public void setDataNascimento(String dataNascimento) {
+    this.dataNascimento = dataNascimento;
+  }
+}
