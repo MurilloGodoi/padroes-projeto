7a96090b0e075705a26d40da265fbfcb6f51a959
Murillo <murillogodoi13@hotmail.com>
2021-04-01 18:25:27 -0300

    fix ConnectionFactory

--- src/main/conexao/ConnectionFactory.java
+++ src/main/conexao/ConnectionFactory.java
@@ -1,17 +1,16 @@
 package conexao;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-
-import javax.management.RuntimeErrorException;
 
 public class ConnectionFactory {
   public Connection getConnection() {
     try {
       return DriverManager.getConnection("jdbc:mysql://localhost/d2mbank","root","password");
     } catch (SQLException e) {
-      e.printStackTrace();
-      throw new RuntimeErrorException(e);
+      e.getMessage();
     }
+    return null;
   }
 }
